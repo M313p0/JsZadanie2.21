f91add2d25bfc01af97f1247152edf71b1cd6654
M313p0 <163188536+M313p0@users.noreply.github.com>
2024-03-18 22:45:54 +0300

    Add files via upload

--- Main1.java
+++ Main1.java
@@ -1,13 +1,13 @@
 import java.util.Scanner;
-public class Main1 {
-    public static void main(String[] args) {
+public class Main1 
+{
+    public static void main(String[] args)
+    {
         Scanner scanner = new Scanner(System.in);
         System.out.println("Введите строку:");
         String input = scanner.nextLine();
-
         input = input.replace("кака", "вырезано цензурой");
         input = input.replace("бяка", "вырезано цензурой");
-
         System.out.println("Результат замены: " + input);
     }
 }
